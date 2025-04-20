a7a3a6f96ca17ad961a3d72e71791a6e723640b1
Raeem Mohammed <36825398+RaeemM@users.noreply.github.com>
2025-04-20 13:17:17 -0400

    Set up firebase project

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCggsZgOrCVNTeYVslzCTrV1xOovxkaXnk",
+  authDomain: "info1601-u-link.firebaseapp.com",
+  projectId: "info1601-u-link",
+  storageBucket: "info1601-u-link.firebasestorage.app",
+  messagingSenderId: "488113160704",
+  appId: "1:488113160704:web:7537d1c0e9ed0ab29acee5"
+};
+
+
+  export default firebaseConfig;
---
+++ readme.md
@@ -0,0 +1,22 @@
+# INFO 1601 Firebase Lab
+
+## Setup
+```
+npm install
+```
+
+## Initialize
+```
+firebase init
+```
+
+## Run
+
+```
+firebase serve
+```
+
+## Deploy
+```
+firebase deploy
+```
